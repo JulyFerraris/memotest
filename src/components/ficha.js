3fd552043fb330759c3cf5ad84d6d6db0d96054f
mblanco <jferraris@garbarino.com.ar>
2020-05-21 20:05:16 -0300

    ajustes

--- src/components/ficha.js
+++ src/components/ficha.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import './ficha.css'
 
-
-{/*
+/*
 const Ficha = () => {
 	_cardClicked = () => alert(this.props.cardLabel);
 	return <div onClick={this._cardClicked} className="ficha"> 
     {this.props.isVisible ? this.props.cardLabel : '?' }
   </div>
-}*/}
-
-
+}*/
 
 class Ficha extends React.Component {   
 
-  	_cardClicked = () => alert(this.props.cardLabel)
+  	_cardClicked = () => {
+		this.props.coordenadas()
+		alert(this.props.cardLabel)
+	}
   
   	render(){
-		return <div className="ficha"
-			onClick={() => {this.props.coordenadas(); this._cardClicked();}} > 
-      		{this.props.isVisible ? this.props.cardLabel : '?' }
+		return <div className="ficha" onClick={this._cardClicked} > 
+      	{this.props.isVisible ? this.props.cardLabel : '?' }
     	</div>
- 	 }
+ 	}
 }
 
 export default Ficha
--- src/components/tablero.js
+++ src/components/tablero.js
@@ -9,32 +9,32 @@ class Tablero extends React.Component {
    constructor(props) {
     	super(props);
     	this.state = {
-      	tablero: [['A','B','A','F'],['D','D','B','C'],['E','F','C','E']],
+         tablero: [['A','B','A','F'],['D','D','B','C'],['E','F','C','E']],
+         fichasClickeadas: []
     	};
   	}
 
-   
+   _clickEnFicha = (coorX,coorY) => {
+	  	//console.log("coordenada " + coorX + "-" + coorY )
+      this.setState({
+			fichasClickeadas: [ ...this.state.fichasClickeadas, [coorX,coorY ]]
+		})
+		console.log("clickeadas", this.state.fichasClickeadas)
+   }
 
 	render(){
-
-      const ClickEnFicha = (coorX,coorY) => {
-         console.log("coordenada " + coorX + "-" + coorY )
-      }
-
 		return <div className="tablero">
 			{ 
-				this.state.tablero.map(function(row, x) {
+				this.state.tablero.map((row, x) => {
   					return (
-						  <div className="tablero-row"
-                        key={x}
-                        > 
+						<div className="tablero-row" key={x} > 
 		  					{ 
-		  						row.map(function(card, y) {
+		  						row.map( (card, y) => {
                            return <Ficha 
 										cardLabel={card} 
 										key={y} 
 										isVisible={false}
-                              coordenadas={() => ClickEnFicha(x, y)}
+										coordenadas={() => this._clickEnFicha(x, y)}
                            />;		    						
 								})
 		  					}
