87aad686d7d5c7ccd70572245387a14bad0705b2
mblanco <jferraris@garbarino.com.ar>
2020-07-21 17:01:56 -0300

    avances

--- server/server.js
+++ server/server.js
@@ -12,6 +12,13 @@ app.get('/api/prueba', (req, res) => {
    })
 });
 
+app.post('/api/prueba', (req, res) => {
+   let contenido = req.body
+   res.send(contenido)
+});
+
+
+
 app.get('/api/tablero', (req, res) => {
    res.send({
       tablero: [['A','B'],['C','B'],['C','A']]
@@ -19,3 +26,50 @@ app.get('/api/tablero', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
+
+
+
+
+let libros = [{ 'id': 0, 'name': 'Dracula'},{ 'id': 1, 'name': '1984'} ]
+
+
+app.get('/api/libros', (req, res) => {
+   res.send(libros)
+});
+
+
+app.post('/api/libros', (req, res) => {
+   let nombre_libro = req.body.name
+   libros.push({'id': libros.length, 'name': nombre_libro})
+   res.send(libros[libros.length - 1])
+});
+
+
+app.get('/api/libros/:id', (req, res) => {
+   const id = req.params.id
+   res.send(libros[id])
+});
+
+
+
+
+app.put('/api/libros/:id', (req, res) => {
+   const id = req.params.id
+   let nombre_libro = req.body.name
+   for( let i=0; i<libros.length; i++){
+      if( libros[i].id === id ){
+         libros[i].name = nombre_libro;
+      }
+   }
+   res.send(libros[id])
+});
+
+app.delete('/api/libros/:id', (req, res) => {
+   const id = req.params.id
+   for( let i=0; i<libros.length; i++){
+      if( libros[i].id === id ){
+         libros.splice(i,1);
+      }
+   }
+   res.send(libros)
+});
